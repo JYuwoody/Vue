789dcb2851598c6e5e77dc7da57e021ba612ce1c
JYuwoody <zx0935697850@gmail.com>
2024-01-07 06:19:47 -0800

    Conditional Rendering

--- app.js
+++ app.js
@@ -3,16 +3,15 @@
     // template: '<h2>I am the template</h2>'
     data() {
         return {
+            showBooks: true,
             title: 'The Final Empire',
             author: 'Tony Lee',
             age: '32'
         }
     },
     methods: {
-        changeTitle(title) {
-            // this.title = 'Words of Randiance'
-            this.title = title
-            console.log('you clicked me')
+        toggleShowBooks() {
+            this.showBooks = !this.showBooks
         }
     }
 
