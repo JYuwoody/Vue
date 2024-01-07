eca1fdf9389a0f059210114f140dfdb838ea3b61
JYuwoody <zx0935697850@gmail.com>
2024-01-07 05:21:28 -0800

    vue init

---
+++ app.js
@@ -0,0 +1,17 @@
+ const app = Vue.createApp({
+    // data, functions
+    // template: '<h2>I am the template</h2>'
+    data() {
+        return {
+            title: 'The Final Empire',
+            author: 'Tony Lee',
+            age: '32'
+        }
+    }
+
+ })
+
+ app.mount('#app')
+ 
+ 
+ console.log("hello vue");
