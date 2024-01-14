3fa4c1d0d336ff5731d7739a1712082fd628eac7
JYuwoody <zx0935697850@gmail.com>
2024-01-14 03:44:19 -0800

    Attribute Binding

--- app.js
+++ app.js
@@ -3,11 +3,12 @@
     // template: '<h2>I am the template</h2>'
     data() {
         return {
+            url: 'https://github.com/JYuwoody',
             showBooks: true,
             books: [
-                { title: 'name of the wind', author: 'partrick rothfuss'},
-                { title: 'the way of kings', author: 'brandon sanderson'},
-                { title: 'the final empire', author: 'brandon sanderson'},
+                { title: 'name of the wind', author: 'parrick rothfuss', img: 'assets/1.jpg' },
+                { title: 'the way of kings', author: 'brandon sanderson', img: 'assets/2.jpg' },
+                { title: 'the final empire', author: 'brandon sanderson', img: 'assets/3.jpg' },
             ]
         }
     },
