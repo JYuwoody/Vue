689ff943c245702cf5abbe272c74aee50c2542d6
JYuwoody <zx0935697850@gmail.com>
2024-01-14 03:14:20 -0800

    Outputting Lists

--- app.js
+++ app.js
@@ -4,27 +4,17 @@
     data() {
         return {
             showBooks: true,
-            title: 'The Final Empire',
-            author: 'Tony Lee',
-            age: '32',
-            x: 0,
-            y: 0
+            books: [
+                { title: 'name of the wind', author: 'partrick rothfuss'},
+                { title: 'the way of kings', author: 'brandon sanderson'},
+                { title: 'the final empire', author: 'brandon sanderson'},
+            ]
         }
     },
     methods: {
         toggleShowBooks() {
             this.showBooks = !this.showBooks
         },
-        handleEvent(e, data) {
-            console.log(e, e.type)
-            if(data){
-                console.log(data)
-            }
-        },
-        handleMousemove(e) {
-            this.x = e.offsetX
-            this.y = e.offsetY
-        }
     }
 
  })
