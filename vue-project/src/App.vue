fcc92e6b81c050e7d5c34cafed254975793c9e5a
JYuwoody <zx0935697850@gmail.com>
2024-01-27 22:48:23 -0800

    Template refs

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -5,42 +5,34 @@ export default {
     return {
       title: 'My first Vue App :)'
     }
+  },
+  methods: {
+    handleClick() {
+      console.log(this.$refs.name)
+      this.$refs.name.classList.add('active')
+      this.$refs.name.focus()
+    }
   }
 }
 </script>
 
 <template>
   <h1> {{ title }}</h1>
+  <input type="text" ref="name">
+  <button @click="handleClick">click me</button>
 
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
+<style>
+#app {
+  font-family: Avenir, Arial, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 h1 {
   border-bottom: 1px solid #ddd;
   display: inline-block;
