8cba4fdb7797cc494cb86e79162b35761aada488
JYuwoody <zx0935697850@gmail.com>
2024-01-27 22:34:41 -0800

    Start Vue CLI

---
+++ vue-project/src/App.vue
@@ -0,0 +1,49 @@
+<script>
+export default {
+  name: 'App',
+  data() {
+    return {
+      title: 'My first Vue App :)'
+    }
+  }
+}
+</script>
+
+<template>
+  <h1> {{ title }}</h1>
+
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+h1 {
+  border-bottom: 1px solid #ddd;
+  display: inline-block;
+  padding-block: 10px;
+}
+</style>
