7522175409e2e95dc28d4d4d487ac6a004f8a5dd
JYuwoody <zx0935697850@gmail.com>
2024-02-14 07:18:54 -0800

    Router Links

--- router-test/src/views/HomeView.vue
+++ router-test/src/views/HomeView.vue
@@ -1,18 +1,14 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Homepage</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione sint ad, laborum, magni beatae omnis saepe iure suscipit odit facere in assumenda, asperiores harum iste incidunt eius dolorum libero sequi?</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione sint ad, laborum, magni beatae omnis saepe iure suscipit odit facere in assumenda, asperiores harum iste incidunt eius dolorum libero sequi?</p>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
   name: 'HomeView',
-  components: {
-    HelloWorld
-  }
+  components: {}
 }
 </script>
