0f8e7879e9715e9c533c8f165e20ae0e40d7027a
JYuwoody <zx0935697850@gmail.com>
2024-02-15 23:30:00 -0800

    Refs vs Reactive

--- blog-test/src/views/HomeView.vue
+++ blog-test/src/views/HomeView.vue
@@ -1,32 +1,39 @@
 <template>
   <div class="home">
     <h1> Home  </h1>
-    <p>My name is {{ name }} and my ages is {{ age }}</p>
-    <button @click="handleClick">click me</button>
-    <button @click="age++">add one age</button>
-    <input type="text" v-model="name">
+    <h2> Refs </h2>
+    <p>{{ woodyOne.name }} - {{ woodyOne.age }}</p>
+    <button @click="updatewoodyOne"> Update Woody One</button>
+    <h2> Reactive</h2>
+    <p>{{ woodyTwo.name }} - {{ woodyTwo.age }} - {{ nameTwo }}</p>
+    <button @click="updatewoodyTwo"> Update Woody Two</button>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import { ref } from 'vue'
+import { ref, reactive } from 'vue'
 
 export default {
   name: 'HomeView',
   setup() {
-    // const p = ref('hello')
+    const woodyOne = ref({ name: 'mario', age: 20})
+    const woodyTwo = reactive({name: 'luigi', age: 25 })
 
-    const name = ref('mario')
-    const age = ref(30)
-    
-    const handleClick = () => {
-      name.value = 'luigi'
-      age.value = 35
+
+    const nameOne = ref('mario')
+    const nameTwo = reactive('luigi')
+
+
+    const updatewoodyOne =() => {
+      woodyOne.value.age = 30
     }
 
-    // return { name: name, age: age}
-    return { name, age, handleClick}
+    const updatewoodyTwo =() => {
+      woodyTwo.age = 30
+      nameTwo = 'mario' // It can not update
+    }
+    return { woodyOne, updatewoodyOne, woodyTwo, updatewoodyTwo, nameTwo}
   },
   data() {
     return {
