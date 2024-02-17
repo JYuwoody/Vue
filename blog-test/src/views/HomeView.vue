85cd9fe5d7847a954b219bd44618101b30584d86
JYuwoody <zx0935697850@gmail.com>
2024-02-16 21:15:43 -0800

    Fetching Data in Setup

--- blog-test/src/views/HomeView.vue
+++ blog-test/src/views/HomeView.vue
@@ -1,9 +1,12 @@
 <template>
   <div class="home">
     <h1> Home  </h1>
-    <PostList v-if="showPosts" :posts="posts" />
-    <button @click="showPosts = !showPosts" >toggle posts</button>
-    <button @click="posts.pop()" >delete posts</button>
+    <div v-if="error">{{ error }}</div>
+    <div v-if="posts.length">
+      <PostList :posts="posts" />
+    </div>
+    <div v-else>Loading...</div>
+    
   </div>
 </template>
 
@@ -16,14 +19,26 @@ export default {
   name: 'HomeView',
   components: { PostList },
   setup() {
-    const posts = ref([
-      { title: 'welcome to the blog', body: 'Lorem ipsum', id: 1},
-      { title: 'top 5 tips', body: 'lorem ipsum', id: 2},
-    ])
-    const showPosts = ref(true)
+    const posts = ref([])
+    const error = ref(null)
+    // http://192.168.159.129:8080/
+    const load = async () => {
+      try {
+        let data = await fetch('http://192.168.159.129:3000/posts') // json-server --watch data/db.json
+        if (!data.ok) {
+          throw Error('no data available')
+        }
+        posts.value = await data.json()
+      }
+      catch (err) {
+        error.value = err.message
+        console.log(error.value)
+      }
+    }
 
+    load()
 
-    return { posts, showPosts }
+    return { posts, error }
   }
 }
 </script>
